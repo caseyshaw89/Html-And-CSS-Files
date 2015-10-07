cf2caf79f7cf26049edae9aec779bc43346419f0
caseyshaw04 <cjs8904@icloud.com>
2015-10-07 10:27:33 -0700

    Added first file
    File I wrote for Tech Academy

---
+++ faqTemplate.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<?php
+/*
+Template Name: FAQ Page
+*/
+?>
+
+<html lang="en">
+	<head>
+		
+		<style type="text/css">
+		#title { font-size .3in; letter-spacing: 5px; margin: 0 auto; position: relative; width: 310px;}
+	   body{ background-color: white;}
+
+  
+        section {background: white ; width: 410px; position: relative; padding: 20px 20px 20px; margin:0 auto;}
+		fieldset {background-color: #59bcd9; position: relative; }
+         #what { display: block; margin: 0 auto; position: relative; width: 310px;text-shadow: 0 1px #59bcd9;float: right; }
+		 #leo {margin: 0 auto; width: 300px; height: 300px; float: left; }
+		 .littleleo {border-style: sold; border-color: red; width 310px; margin: 0 auto;}
+		</style>
+		<meta charset="utf-8">
+
+		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
+		Remove this if you use the .htaccess -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+		<title>FAQ</title>
+		<meta name="description" content="">
+		<meta name="author" content="st">
+
+		<meta name="viewport" content="width=device-width; initial-scale=1.0">
+
+		
+	</head>
+
+	<body>
+		    <fieldset>
+			<header id="title">
+				<h1>Frequently asked questions</h1>
+			</header>
+			</fieldset>
+            <aside>
+			<img id="leo" src="http://caseyjshaw.prosperitacademy.com/wp-content/uploads/2015/08/IMG_3523-169x300.png" alt="IMG_3523"/>
+			</aside>
+	         
+			 
+			 
+			 
+			 
+			 <div id="what">
+			 <nav>
+			 <h2>Helpful Links...</h2>
+			  
+			    <a href="http://dogtime.com/dog-breeds/akita"><p><i>This site will help you learn more about your dog</i></p></a>
+				<a href="http://www.dogbreedinfo.com/akita.htm"><p><i>This is another site that I would recommend to anyone.</i></p></a>
+				<a href="http://www.yourpurebredpuppy.com/reviews/akitas.html"><p><i>If you are consdering a puppy, read this site</i></p></a>
+			</nav>
+			  </div>
+			<section>
+               <h2>What is your own personal experience with akitas?</h2>
+			  <blockquote><p>My experience with these kinds of animals is vast. At an early age we were interduced to this breed. Our mother was a dog breeder, and taught us the ins and outs of these animals.</p></blockquote>
+			</section>
+           <section> 
+           	<h2>What kind of dog food should I be feeding my akita?</h2>
+           	 <blockquote> <p>Akitas are susceptible to bloat, and should be monitered properly while eating. One way of preventing bloat is to feed them grain free dog food. While its more expensive, it can possibly save your dogs life.</p></blockquote>
+           	 </section>
+			<section>
+			  <h2>With this information, will I be able to properly own an akita?</h2>
+			    <blockquote><p>Essentially yes, but owning an Akita is not like owning a smaller breed. With inproper ownership, your dog could be a hazard to everyone involved. They are wonderful dogs, but are also very powerful dogs.</p></blockquote>
+			</section>
+              <div class="littleleo">
+			  		   
+			<form method="post" name="contact_form" action="contact-form-handler.php">
+               
+			   <h2>Any Questions please contact us</h2>
+             Your Name:
+              <input type="text" name="name">
+                Email Address:
+                 <input type="text" name="email">
+                     Message:
+                <textarea name="message"></textarea>
+                <input type="submit" value="Submit">
+                
+				</form> 
+             </div>
+			 <fieldset>
+			<footer>
+				
+				<p>
+					&copy; Copyright  by Casey Shaw
+				</p>
+			</footer>
+		    </fieldset>
+	</body>
+</html>
